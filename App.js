0f9ceda7b531d9b8445f568762b8fe5417d19e3a
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-08 16:48:05 +0800

    add first stream chat

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, SafeAreaView } from "react-native";
+import { StreamChat } from "stream-chat";
+import {
+  Chat,
+  Channel,
+  MessageList,
+  MessageInput,
+} from "stream-chat-expo";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const chatClient = new StreamChat('f8wwud5et5jd');
+const userToken =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoidGlueS1sYWItOSJ9.xxi2iXxKgFyDnkDHqVr56MU2wd37DRx3dKTzNUSZOx4';
+
+const user = {
+  id: 'tiny-lab-9',
+  name: 'Tiny lab',
+  image:
+    'https://stepupandlive.files.wordpress.com/2014/09/3d-animated-frog-image.jpg',
+};
+
+chatClient.setUser(user, userToken);
+
+class ChannelScreen extends React.Component {
+  render() {
+    const channel = chatClient.channel("messaging", "tiny-lab-9");
+    channel.watch();
+
+    return (
+      <SafeAreaView>
+        <Chat client={chatClient}>
+          <Channel channel={channel}>
+            <View style={{ display: "flex", height: "100%" }}>
+              <MessageList />
+              <MessageInput />
+            </View>
+          </Channel>
+        </Chat>
+      </SafeAreaView>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class App extends React.Component {
+  render() {
+    return <ChannelScreen />;
+  }
+}
+    
